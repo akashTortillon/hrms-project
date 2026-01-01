a599cc4114e0ddfcf41546c5dcbc399731e72211
anoop2000 <88547121+anoop2000@users.noreply.github.com>
2026-01-01 23:14:05 +0530

    usermodel updated

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -4,6 +4,15 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  phone: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\+\d{12}$/,
+        "Phone number must include country code and be 12 digits (e.g. +971501234567)"
+      ]
+    },
   password: { type: String, required: true },
 },
 { timestamps: true });
