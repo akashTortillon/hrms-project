d91d398d89da3051ad12e8e69158d387b1831b2e
anoop2000 <88547121+anoop2000@users.noreply.github.com>
2026-01-06 15:35:44 +0530

    allowed origin updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,8 @@ const app = express();
 // ✅ Allowed frontend origins
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://hr-and-asset-management-zmpu.vercel.app"
+  "https://hr-and-asset-management-zmpu.vercel.app",
+  "https://hr-and-asset-management-7yzy.vercel.app"
 ];
 
 // ✅ CORS middleware (must be BEFORE routes)
