00d0cc23dbf31f037c60dd0cf9fb7d304740f18b
anoop2000 <88547121+anoop2000@users.noreply.github.com>
2026-01-08 00:21:15 +0530

    toast message added successfully

--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -8,11 +8,11 @@ import { protect } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 // GET all employees
-router.get("/",  getEmployees);
+router.get("/", protect, getEmployees);
 console.log("✅ GET /api/employees route registered");
 
 // ADD new employee
-router.post("/", addEmployee);
+router.post("/", protect, addEmployee);
 console.log("✅ POST /api/employees route registered");
 
 export default router;
