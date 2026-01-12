fad0fa0c7d9b354c1656cfcdbde6b5539f5c67de
hrithikdas7 <hrithikdas044gmail.com>
2026-01-13 02:23:02 +0530

    implemented-companystructure-Apis

--- hr_and_asset_mgt/src/components/reusable/Card.jsx
+++ hr_and_asset_mgt/src/components/reusable/Card.jsx
@@ -1,13 +1,16 @@
 import { Card as RBCard } from "react-bootstrap";
 import "../../style/layout.css";
 
-export default function Card({ title, subtitle, children, className = "" }) {
+export default function Card({ title, subtitle, rightAction, children, className = "" }) {
   return (
     <RBCard className={`card-container ${className}`}>
-      {(title || subtitle) && (
-        <RBCard.Header className="card-header">
-          <div className="card-title">{title}</div>
-          {subtitle && <div className="card-subtitle">{subtitle}</div>}
+      {(title || subtitle || rightAction) && (
+        <RBCard.Header className="card-header d-flex justify-content-between align-items-center">
+          <div>
+            {title && <div className="card-title">{title}</div>}
+            {subtitle && <div className="card-subtitle">{subtitle}</div>}
+          </div>
+          {rightAction && <div className="card-action">{rightAction}</div>}
         </RBCard.Header>
       )}
       <RBCard.Body className="card-body">{children}</RBCard.Body>
